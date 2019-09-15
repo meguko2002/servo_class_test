ed25d0e8ac6e338a2f02f4826401670d7698bd0d
meguko <meguko2002@gmail.com>
2019-09-15 14:51:23 +0900

    参照渡し。　アドレスをシリアルモニタに表示して、2つのアドレス "&a[1]" と "a+1"は同じであることを検証

--- servo_class_test.ino
+++ servo_class_test.ino
@@ -2,23 +2,30 @@
 Servo myservo[2];    //静的にメモリを確保する必要がある([]はNG)
 const int len = sizeof(myservo) / sizeof(myservo[0]);
 
-void turn(Servo servo) {
+void turn(const Servo &servo) {
+  /*参照渡し, 元の変数に別名をつける記法
+    変更すると呼び出し元の変数も書き換えられる*/
   int pos;
-  for (pos = 0; pos <= 180; pos += 1) turn_d(pos,servo);
-  for (pos = 180; pos >= 0; pos -= 1) turn_d(pos,servo);
+  for (pos = 0; pos <= 180; pos += 1) turn_d(pos, servo);
+  for (pos = 180; pos >= 0; pos -= 1) turn_d(pos, servo);
 }
 
 void turn_d(int pos, Servo servo) {
   servo.write(pos);
-  Serial.println(servo.read());
   delay(15);
 }
 
 void setup() {
   Serial.begin(9600);
+  char buffer[30];
   for (int i = 0; i < len; i++) {
     myservo[i].attach(8 + i);
   }
+  int j = 0;
+  sprintf(buffer, " &myservo[0] = %p, &myservo[1] = %p", &myservo[0], &myservo[1]);
+  Serial.println(buffer);
+  sprintf(buffer, "  myservo    = %p,  myservo + 1= %p", myservo, myservo + 1);
+  Serial.println(buffer);
   for (int i = 0; i < len; i++) {
     turn(myservo[i]);
     myservo[i]. detach();
